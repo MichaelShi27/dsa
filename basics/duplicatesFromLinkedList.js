4ad2ec9a9117395d6c0849c5b995169bedcda0d0
Michael Shi <michaelshi03@gmail.com>
2021-03-03 22:52:40 -0800

    Implement nonConstructibleChange & duplicatesFromLinkedList

---
+++ basics/duplicatesFromLinkedList.js
@@ -0,0 +1,48 @@
+const removeDuplicatesFromLinkedList = list => {
+  let prev = list;
+  let curr = list.next;
+
+  while (prev.next) {
+    if (prev.value === curr.value) prev.next = curr.next // remove curr from LL
+    else prev = curr;
+    curr = curr.next;
+  }
+  return list;
+};
+
+
+// TESTING
+
+class LinkedList {
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
+}
+
+let one = new LinkedList(1);
+let two = new LinkedList(1);
+let three = new LinkedList(3);
+let four = new LinkedList(4);
+let five = new LinkedList(4);
+let six = new LinkedList(4);
+let seven = new LinkedList(5);
+let eight = new LinkedList(6);
+let nine = new LinkedList(6);
+
+one.next = two;
+two.next = three;
+three.next = four;
+four.next = five;
+five.next = six;
+six.next = seven;
+seven.next = eight;
+eight.next = nine;
+
+// let curr = one;
+let curr = removeDuplicatesFromLinkedList(one);
+
+while (curr) {
+  console.log(curr.value);
+  curr = curr.next;
+}
--- basics/nonConstructibleChange.js
+++ basics/nonConstructibleChange.js
@@ -1,8 +1,19 @@
 const nonConstructibleChange = coins => {
   coins = coins.sort((a, b) => a - b);
-  let count = 1;
+  let currentMax = 0;
 
-  while(true) {
-    count++;
+  if (coins[0] !== 1) return 1;
+
+  for (let i = 0; i < coins.length; i++) {
+    currentMax += coins[i];
+    if (coins[i + 1] > currentMax + 1) break;
   }
-};
+  return currentMax + 1;
+}
+
+let coins = [ 5, 7, 1, 1, 2, 3, 22 ]; // 20
+let coins = [ 5, 1, 2 ]; // 4
+console.log( nonConstructibleChange(coins) );
+
+
+// can also solve by switching 8 & 9, removing 5, changing coins[i + 1] to [i], return currentMax + 1 inside for loop
