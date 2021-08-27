6ad1ed2a162046196f1b26511ebaaac365867d3c
Michael Shi <michaelshi03@gmail.com>
2021-08-26 19:10:54 -0700

    Implement radixSort for positive integers

--- udemy/radixSort.js
+++ udemy/radixSort.js
@@ -14,7 +14,7 @@
 //   return mod;
 // };
 
-const getDigit = (num, place) => Math.floor( num / (10 ** place) % 10 );
+const getDigit = (num, place) => Math.floor(num / (10 ** place)) % 10;
 
 // console.log( getDigit(43210, 0) );
 // console.log( getDigit(43210, 2) );
@@ -22,15 +22,25 @@ const getDigit = (num, place) => Math.floor( num / (10 ** place) % 10 );
 // console.log( getDigit(43210, 5) );
 // console.log( getDigit(43210, 10) );
 
-// const radixSort = arr => {
-//   const bucket0 = [];
-//   const bucket1 = [];
-//   const bucket2 = [];
-//   const bucket3 = [];
-//   const bucket4 = [];
-//   const bucket5 = [];
-//   const bucket6 = [];
-//   const bucket7 = [];
-//   const bucket8 = [];
-//   const bucket9 = [];
-// };
+const radixSort = arr => {
+    let place = 0;
+    while (true) {
+      const buckets = { 0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: [], 7: [], 8: [], 9: [] };
+      for (let el of arr)
+        buckets[getDigit(el, place)].push(el);
+      place++;
+
+      arr = [];
+      for (let i = 0; i < 10; i++)
+        for (let el of buckets[i])
+          arr.push(el);
+      if (buckets[0].length === arr.length) break;
+    }
+    return arr;
+};
+
+// const arr = [ 3, 2, 9, 1, 4 ];
+// const arr = [ 5, 22, 1, 7, 3, 2, 8, 4, 9 ];
+const arr = [ 9, 3, 11, 111, 3, 901, 8938398, 888, 2989, 1083038389383833333, 11900000, 30901, 898 ];
+// const arr = [ 22, -1, 80, -33, 3, 0, 8, 11, 0, 56, 5, 22, 1, 7, 3, 2, 8, 4, 9 ];
+console.log(radixSort(arr));
