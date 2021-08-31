37d835c292666a486b0e8471af35f75ec50148f0
Michael Shi <michaelshi03@gmail.com>
2021-08-31 00:24:20 -0700

    Implement shift & unshift for singlyLinkedList

--- udemy/singlyLinkedList.js
+++ udemy/singlyLinkedList.js
@@ -32,17 +32,42 @@ class SinglyLinkedList {
       prev = curr;
       curr = curr.next;
     }
-    if (prev) // in case we're removing the sole node
+    if (prev) // in case we're removing the sole node (null.next would give error)
       prev.next = null;
     this.tail = prev;
     this.length--;
+    if (this.length === 0)
+      this.head = null;
     return curr.val;
+  }
+
+  shift() {
+    if (!this.head)
+      return;
+    const val = this.head.val;
+    this.head = this.head.next;
+    this.length--;
+    if (this.length === 0)
+      this.tail = null;
+    return val;
+  }
+
+  unshift(val) {
+    const node = new Node(val);
+    node.next = this.head;
+    this.head = node;
+    this.length++;
+    if (this.length === 1)
+      this.tail = node;
+    return this;
   }
 }
 
 const newList = new SinglyLinkedList();
-newList.push('7');
-newList.push('8');
-newList.push('9');
-newList.pop();
+// newList.push('7');
+// newList.push('8');
+// newList.push('9');
+newList.unshift('9');
+// console.log( newList.pop() );
+// console.log( newList.pop() );
 console.log(newList, newList.length);
