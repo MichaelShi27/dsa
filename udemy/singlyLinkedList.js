ad0824943c5e67b1c89d181f21858d8f97176644
Michael Shi <michaelshi03@gmail.com>
2021-08-31 17:42:13 -0700

    Implement insert & remove for singlyLinkedList

--- udemy/singlyLinkedList.js
+++ udemy/singlyLinkedList.js
@@ -26,18 +26,22 @@ class SinglyLinkedList {
   pop() {
     if (!this.head)
       return;
+
     let curr = this.head;
     let prev = null;
     while (curr.next) {
       prev = curr;
       curr = curr.next;
     }
-    if (prev) // in case we're removing the sole node (null.next would give error)
+
+    if (prev) // needed in case we're removing the sole node (null.next would give error)
       prev.next = null;
     this.tail = prev;
+
     this.length--;
     if (this.length === 0)
       this.head = null;
+
     return curr.val;
   }
 
@@ -61,13 +65,63 @@ class SinglyLinkedList {
       this.tail = node;
     return this;
   }
+
+  get(idx) {
+    if (!this.head || idx < 0 || idx >= this.length)
+      return;
+    let curr = this.head;
+    while (idx--)
+      curr = curr.next;
+    return curr;
+  }
+
+  set(idx, val) {
+    const node = this.get(idx);
+    if (!node)
+      return false;
+    node.val = val;
+    return true;
+  }
+
+  insert(idx, val) {
+    if (idx < 0 || idx > this.length)
+      return false;
+    if (idx === 0)
+      return !!this.unshift(val);
+    if (idx === this.length)
+      return !!this.push(val);
+
+    const prev = this.get(idx - 1);
+    const next = prev.next;
+    prev.next = new Node(val);
+    prev.next.next = next;
+
+    this.length++;
+    return true;
+  }
+
+  remove(idx) {
+    if (idx < 0 || idx >= this.length)
+      return false;
+    if (idx === 0)
+      return !!this.shift();
+    if (idx === this.length - 1)
+      return !!this.pop();
+
+    const prev = this.get(idx - 1);
+    const removed = prev.next;
+    prev.next = removed.next;
+
+    this.length--;
+    return removed;
+  }
 }
 
 const newList = new SinglyLinkedList();
 // newList.push('7');
 // newList.push('8');
 // newList.push('9');
-newList.unshift('9');
-// console.log( newList.pop() );
-// console.log( newList.pop() );
+// newList.unshift('6');
+console.log( newList.insert(0, 0) );
 console.log(newList, newList.length);
+console.log(newList.head.next.next);
