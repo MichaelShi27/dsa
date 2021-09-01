4e86e79a799b01eaff3dfa2a18fa4d515320e7d2
Michael Shi <michaelshi03@gmail.com>
2021-08-31 22:02:07 -0700

    Implement shift() & unshift() for doublyLinkedList

--- udemy/doublyLinkedList.js
+++ udemy/doublyLinkedList.js
@@ -38,8 +38,45 @@ class DoublyLinkedList {
       popped.prev = null;
       this.tail.next = null;
     }
-    length--;
+    this.length--;
     return popped;
   }
 
+  shift() {
+    if (!this.head)
+      return;
+    const shifted = this.head;
+    this.head = shifted.next;
+    if (this.length === 1)
+      this.tail = null;
+    else {
+      shifted.next = null;
+      this.head.prev = null;
+    }
+    this.length--;
+    return shifted;
+  }
+
+  unshift(val) {
+    const node = new Node(val);
+    if (!this.head)
+      this.tail = node;
+    else {
+      node.next = this.head;
+      this.head.prev = node;
+    }
+    this.head = node;
+    this.length++;
+    return this;
+  }
 }
+
+const newList = new DoublyLinkedList();
+newList.push('7');
+newList.push('8');
+newList.push('9');
+newList.unshift('6');
+newList.unshift('5');
+newList.pop();
+newList.shift();
+console.log(newList, newList.length);
