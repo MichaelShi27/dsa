b57e522d26be25a4bcbff3c28b01a08851a45be0
Michael Shi <michaelshi03@gmail.com>
2021-09-12 15:22:34 -0700

    Implement get() & set() for hashTable

--- udemy/hashTable.js
+++ udemy/hashTable.js
@@ -3,11 +3,33 @@ class HashTable {
     this.keyMap = new Array(size);
   }
 
-  _hash(key, len) {
+  _hash(key) {
+    const WEIRD_PRIME = 31;
     let sum = 0;
-    let WEIRD_PRIME = 31;
     for (let i = 0; i < Math.min(key.length, 100); i++)
-      sum += key.charCodeAt(i) -  96;
-    return sum % len;
+      sum = sum * WEIRD_PRIME + key.charCodeAt(i) -  96;
+    return sum % this.keyMap.length;
+  }
+
+  set(key, val) {
+    const idx = this._hash(key);
+    if (!this.keyMap[idx])
+      this.keyMap[idx] = [];
+    this.keyMap[idx].push([ key, val ]);
+  }
+
+  get(key) {
+    const idx = this._hash(key);
+    const bucket = this.keyMap[idx];
+    if (bucket)
+      for (let el of bucket)
+        if (el[0] === key) return el[1];
+    return;
   }
 }
+
+const ht = new HashTable();
+ht.set('a', 1);
+ht.set('b', 2);
+console.log(ht.keyMap);
+console.log(ht.get('b'))
