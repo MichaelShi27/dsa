4b3facbc73682b3a0f8e33aef38b4d4e2050bb79
Michael Shi <michaelshi03@gmail.com>
2021-09-14 00:07:32 -0700

    Implement removeEdge() for a graph class

--- udemy/graph.js
+++ udemy/graph.js
@@ -18,6 +18,27 @@ class Graph {
     list[v1].push(v2);
     list[v2].push(v1);
   }
+
+  removeEdge(v1, v2) {
+    const list = this.adjacencyList;
+    if (!list[v1] || !list[v2]) return;
+
+    // // this approach is faster than 2 for-loops/2 .filters when the arrays are very long
+    // let firstSideRemoved, secondSideRemoved;
+    // const maxLength = Math.max(list[v1].length, list[v2].length);
+    // for (let i = 0; i < maxLength && (!firstSideRemoved || !secondSideRemoved); i++) {
+    //   if (!firstSideRemoved && list[v1][i] === v2) {
+    //     list[v1].splice(i, 1);
+    //     firstSideRemoved = true;
+    //   }
+    //   if (!secondSideRemoved && list[v2][i] === v1) {
+    //     list[v2].splice(i, 1);
+    //     secondSideRemoved = true;
+    //   }
+    // }
+    list[v1] = list[v1].filter(val => val !== v2);
+    list[v2] = list[v2].filter(val => val !== v1);
+  }
 }
 
 const g = new Graph();
@@ -26,4 +47,5 @@ g.addVertex('zak');
 g.addVertex('jack');
 g.addEdge('ben', 'jack');
 g.addEdge('ben', 'zak');
+g.removeEdge('ben', 'zak');
 console.log(g.adjacencyList);
