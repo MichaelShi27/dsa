ae049bdd9b2f97bac6502cf647611584a35cc567
Michael Shi <michaelshi03@gmail.com>
2021-10-07 10:39:51 -0700

    Review & implement bubble sort

--- udemy/review.js
+++ udemy/review.js
@@ -250,3 +250,18 @@ const testSort = sort => {
   console.log(testArrayEquality( sort([ 1, 2 ]), [ 1, 2 ] ));
 };
 
+const bubbleSort = arr => {
+  let swapFound = false;
+  for (let i = arr.length - 1; i > 0; i--) {
+    swapFound = false;
+    for (let j = 0; j < i; j++)
+      if (arr[j] > arr[j + 1]) {
+        [ arr[j], arr[j + 1 ] ] = [ arr[j + 1], arr[j] ];
+        swapFound = true;
+      }
+    if (!swapFound) return arr;
+  }
+  return arr;
+};
+
+testSort(bubbleSort);
