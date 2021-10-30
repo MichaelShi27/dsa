9104d0886f39b3c6654eceef4309eedeade20be3
Michael Shi <michaelshi03@gmail.com>
2021-10-30 11:20:56 -0700

    Implement containsDuplicate

---
+++ leetcode/easy/containsDuplicate.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+ var containsDuplicate = function(nums) {
+  const seen = {};
+  for (let num of nums) {
+      if (seen[num]) return true;
+      seen[num] = true;
+  }
+  return false;
+};
