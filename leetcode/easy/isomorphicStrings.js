8eafb04d018e829a269098b7a56f46e898be22c3
Michael Shi <michaelshi03@gmail.com>
2021-11-04 17:10:14 -0700

    Implement isomorphicStrings

--- leetcode/easy/isomorphicStrings.js
+++ leetcode/easy/isomorphicStrings.js
@@ -27,42 +27,20 @@
 // s and t consist of any valid ascii character.
 
 // store each mapping
- var isIsomorphic = function(s, t) {
+var isIsomorphic = function(s, t) {
   const sObj = {};
   const tObj = {};
-
   for (let i = 0; i < s.length; i++) {
-    if ( sObj[s[i]] || tObj[t[i]] ) {
-      if ( sObj[s[i]] !== t[i] || tObj[t[i]] !== s[i] )
-        return false;
-    } else {
-      sObj[s[i]] = t[i];
-      tObj[t[i]] = s[i];
-    }
+      const sChar = s[i];
+      const tChar = t[i];
+      if (tObj[sChar] || sObj[tChar]) {
+          if (sChar !== sObj[tChar] || tChar !== tObj[sChar])
+              return false;
+      } else {
+          sObj[tChar] = sChar;
+          tObj[sChar] = tChar;
+      }
   }
   return true;
 };
 
-// alter strings & compare
-var isIsomorphic = function(s, t) {
-  const sObj = {};
-  const tObj = {};
-  s = s.split('');
-  t = t.split('');
-
-  for (let i = 0; i < s.length; i++) {
-      if ( sObj[s[i]] !== undefined )
-          s[i] = sObj[s[i]];
-      else {
-          sObj[s[i]] = i;
-          s[i] = i;
-      }
-      if ( tObj[t[i]] !== undefined )
-          t[i] = tObj[t[i]];
-      else {
-          tObj[t[i]] = i;
-          t[i] = i;
-      }
-  }
-  return s.join(' ') === t.join(' ');
-};
