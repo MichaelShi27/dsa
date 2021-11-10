53e05cecf056691810968b62d13a4dca24233af1
Michael Shi <michaelshi03@gmail.com>
2021-11-10 01:17:57 -0800

    Implement maximumSubarray

---
+++ leetcode/easy/maximumSubarray.js
@@ -0,0 +1,21 @@
+// 53. Maximum Subarray
+// https://leetcode.com/problems/maximum-subarray/
+
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+// A subarray is a contiguous part of an array.
+
+const maxSubArray = nums => {
+  let maxSum = -Infinity;
+  let curSum = 0;
+
+  for (let start = 0, end = 0; end < nums.length; end++) {
+    curSum += nums[end];
+    maxSum = Math.max(maxSum, curSum);
+    if (curSum < 0) {
+      start = end + 1;
+      curSum = 0;
+    }
+  }
+  return maxSum;
+};
