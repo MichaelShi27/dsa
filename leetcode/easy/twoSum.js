c1adb0bf1528eebe62046fb5ce96a7aa1f1e38b2
Michael Shi <michaelshi03@gmail.com>
2021-11-15 02:17:19 -0800

    Implement two-pointer approach to twoSum

--- leetcode/easy/twoSum.js
+++ leetcode/easy/twoSum.js
@@ -1,41 +1,73 @@
+// 1. Two Sum
+// https://leetcode.com/problems/two-sum/
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+
+// const twoSum = (nums, target) => {
+//   const obj = {};
+
+//   for (let i = 0; i < nums.length; i++)
+//     if (obj[nums[i]] !== undefined)
+//       return [ i, obj[nums[i]] ];
+//     else
+//       obj[target - nums[i]] = i;
+// };
+
+// two-pointer method => less efficient: n log n b/c of .sort()
 const twoSum = (nums, target) => {
   const obj = {};
+  const found = [];
+  const numsCopy = nums.slice();
+  const sortedNums = nums.sort((a, b) => a - b);
 
-  for (let i = 0; i < nums.length; i++) {
-    if (obj[nums[i]] !== undefined)
-      return [ i, obj[nums[i]] ];
-    else
-      obj[target - nums[i]] = i;
+  for (let i = 0, j = sortedNums.length - 1; true; i++, j--) {
+    const lowerEl = sortedNums[i];
+    const higherEl = sortedNums[j];
+
+    if (obj[lowerEl] !== undefined) {
+      found.push(lowerEl, target - lowerEl);
+      break;
+    } else
+      obj[target - lowerEl] = true;
+
+    if (obj[higherEl] !== undefined) {
+      found.push(higherEl, target - higherEl);
+      break;
+    } else
+      obj[target - higherEl] = true;
   }
+
+  let firstIdx, secondIdx;
+  for (let i = 0; firstIdx == undefined || secondIdx === undefined; i++) {
+    if (firstIdx === undefined && numsCopy[i] === found[0])
+      firstIdx = i;
+    else if (numsCopy[i] === found[1])
+      secondIdx = i;
+  }
+  return [ firstIdx, secondIdx ];
 };
 
-
-// var threeSum = function(nums) {
-//   const res = [];
-//   if (nums.length < 3) return res;
-//   const store = {};
-
-//   for (let i = 0; i < nums.length; i++) {
-//       let target = -nums[i];
-//       let obj = {};
-
-//       for (let j = i + 1; j < nums.length; j++) {
-//           if (obj[nums[j]] !== undefined) {
-//               let k = obj[nums[j]];
-//               if (
-//                   obj[nums[j]] !== obj[j] &&
-//                   obj[j] !== obj[i] &&
-//                   store[obj[i]] !== obj[j] &&
-//                   store[obj[i]] !== obj[k]
-//               ) {
-//                   res.push([ obj[i], obj[j], obj[k] ]);
-//                   store[obj[i]] = obj[j];
-//                   store[obj[j]] = obj[i];
-//                   store[obj[k]] = obj[i];
-//               }
-//           } else
-//               obj[target - nums[j]] = j;
-//       }
-//   }
-//   return res;
-// };
+// console.log( twoSum([ -18, 12, 3, 0 ], -6) ); // [ 0, 1 ]
+console.log( twoSum([ 3, 3 ], 6) ); // [ 0, 1 ]
+// console.log( twoSum([ 3, 2, 4 ], 6) ); // [ 1, 2 ]
