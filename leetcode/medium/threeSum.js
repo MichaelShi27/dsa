5e15a29f54718e1f31358c446847de096fec2eb3
Michael Shi <michaelshi03@gmail.com>
2021-11-16 16:48:25 -0800

    Implement threeSum

--- leetcode/medium/threeSum.js
+++ leetcode/medium/threeSum.js
@@ -20,45 +20,42 @@
 // Input: nums = [0]
 // Output: []
 
+// // very slow solution => faster than ~12%
+// const threeSum = nums => {
+//   const seen = {};
+//   const res = [];
 
+//   for (let i = 0; i < nums.length; i++) {
+//     const num = nums[i];
+//     if (seen[num]) // we've already added all possible answers with this num
+//       continue;
 
+//     const twoSumRes = modifiedTwoSum(nums, -num, i + 1);
+//     if (twoSumRes.length)
+//       for (let candidate of twoSumRes)
+//         if (!seen[candidate[0]] && !seen[candidate[1]] && !seen[candidate[2]]) // if it's in 'seen', it's been fully "explored" so we'd be adding duplicate
+//           res.push(candidate);
+//     seen[num] = true;
+//   }
+//   return res;
+// };
 
-// very slow solution => faster than ~12%
-const threeSum = nums => {
-  const seen = {};
-  const res = [];
+// const modifiedTwoSum = (nums, target, startingIdx) => {
+//   const seen = {};
+//   const res = [];
 
-  for (let i = 0; i < nums.length; i++) {
-    const num = nums[i];
-    if (seen[num]) // we've already added all possible answers with this num
-      continue;
+//   for (let i = startingIdx; i < nums.length; i++) {
+//     const num = nums[i];
+//     if (seen[num] === false) // we've already used this num & its counterpart, so we skip to avoid repeated values
+//       continue;
+//     else if (seen[num]) {
+//       res.push([ -target, num, target - num ]);
+//       seen[num] = false;
+//       seen[target - num] = false;
+//     } else
+//       seen[target - num] = true;
+//   }
+//   return res;
+// };
 
-    const twoSumRes = modifiedTwoSum(nums, -num, i + 1);
-    if (twoSumRes.length)
-      for (let candidate of twoSumRes)
-        if (!seen[candidate[0]] && !seen[candidate[1]] && !seen[candidate[2]]) // if it's in 'seen', it's been fully "explored" so we'd be adding duplicate
-          res.push(candidate);
-    seen[num] = true;
-  }
-  return res;
-};
-
-const modifiedTwoSum = (nums, target, startingIdx) => {
-  const seen = {};
-  const res = [];
-
-  for (let i = startingIdx; i < nums.length; i++) {
-    const num = nums[i];
-    if (seen[num] === false) // we've already used this num & its counterpart, so we skip to avoid repeated values
-      continue;
-    else if (seen[num]) {
-      res.push([ -target, num, target - num ]);
-      seen[num] = false;
-      seen[target - num] = false;
-    } else
-      seen[target - num] = true;
-  }
-  return res;
-};
-
-console.log( modifiedTwoSum([-1, 0, 1, 1, 0, 2, -1, -2, -4 ], 1, 1) );
+// console.log( modifiedTwoSum([-1, 0, 1, 1, 0, 2, -1, -2, -4 ], 1, 1) );
