75bff24626dff98f0d03496cc47bda681691e630
Michael Shi <michaelshi03@gmail.com>
2021-11-17 01:40:21 -0800

    Implement alternate approach using Array.sort() for 'Group Anagrams'

--- leetcode/medium/groupAnagrams.js
+++ leetcode/medium/groupAnagrams.js
@@ -1,21 +1,35 @@
 // 49. Group Anagrams
 // https://leetcode.com/problems/group-anagrams/
 
+// uses array of letter counts as key in obj
 const groupAnagrams = strings => {
   const obj = {};
-  for (const string of strings) {
+  for (const str of strings) {
     const letterCounts = new Array(26).fill(0);
-    for (const char of string) {
+    for (const char of str) {
       const idx = char.charCodeAt() - 97;
       letterCounts[idx]++;
     }
     if (obj[letterCounts])
-      obj[letterCounts].push(string);
+      obj[letterCounts].push(str);
     else
-      obj[letterCounts] = [ string ];
+      obj[letterCounts] = [ str ];
   }
   return Object.values(obj);
 };
+
+// // uses sorted string as key in obj
+// const groupAnagrams = strings => {
+//   const obj = {};
+//   for (const str of strings) {
+//     const sortedStr = str.split('').sort();
+//     if (obj[sortedStr])
+//       obj[sortedStr].push(str);
+//     else
+//       obj[sortedStr] = [ str ];
+//   }
+//   return Object.values(obj);
+// };
 
 console.log( groupAnagrams(["eat","tea","tan","ate","nat","bat"]) );
 console.log( groupAnagrams(['']) );
