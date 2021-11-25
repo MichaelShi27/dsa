6b59fe0aa4657836b3cf248513aec3df0cd9fa6b
Michael Shi <michaelshi03@gmail.com>
2021-11-24 21:20:48 -0800

    Implement iterative approach to 'Binary Tree Inorder Traversal'

--- leetcode/easy/binaryTreeInorderTraversal.js
+++ leetcode/easy/binaryTreeInorderTraversal.js
@@ -1,31 +1,51 @@
 // 94. Binary Tree Inorder Traversal
 // https://leetcode.com/problems/binary-tree-inorder-traversal/
 
+// iterative - the stack is only for the left children
 const inorderTraversal = root => {
   const vals = [];
+  const stack = [];
+  let cur = root;
 
-  const recurse = node => {
-    if (!node) return;
-
-    recurse(node.left);
-    vals.push(node.val);
-    recurse(node.right);
+  while (cur || stack.length) {
+    while (cur) {
+      stack.push(cur);
+      cur = cur.left;
+    }
+    cur = stack.pop();
+    vals.push(cur.val);
+    cur = cur.right;
   };
-  recurse(root);
   return vals;
 };
 
-const inorderTraversal = root => {
-  const vals = [];
+// recursive 1
+// const inorderTraversal = root => {
+//   const vals = [];
 
-  const recurse = (node, vals = []) => {
-    if (!node) return vals;
+//   const recurse = node => {
+//     if (!node) return;
 
-    recurse(node.left, vals);
-    vals.push(node.val);
-    recurse(node.right, vals);
+//     recurse(node.left);
+//     vals.push(node.val);
+//     recurse(node.right);
+//   };
+//   recurse(root);
+//   return vals;
+// };
 
-    return vals;
-  };
-  return recurse(root);
-};
+// recursive 2
+// const inorderTraversal = root => {
+//   const vals = [];
+
+//   const recurse = (node, vals = []) => {
+//     if (!node) return vals;
+
+//     recurse(node.left, vals);
+//     vals.push(node.val);
+//     recurse(node.right, vals);
+
+//     return vals;
+//   };
+//   return recurse(root);
+// };
