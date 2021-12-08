afcc639c819ee47a3d3f0f0c9b0564c39a8aaa7f
Michael Shi <michaelshi03@gmail.com>
2021-12-08 12:29:54 -0800

    Improve solution to 'Rotate List'

--- leetcode/medium/rotateList.js
+++ leetcode/medium/rotateList.js
@@ -1,8 +1,8 @@
 // 61. Rotate List
 // https://leetcode.com/problems/rotate-list/
 
-// O(k * n) => exceeds time limit
-const rotateRight = (head, k) => {
+// like rotateRight1, but with a rotateLeft in case it's more efficient to use it
+const rotateRight2 = (head, k) => {
   if (!head || !head.next) return head;
 
   let prev;
@@ -13,11 +13,13 @@ const rotateRight = (head, k) => {
     cur = cur.next;
     len++;
   }
-
   k %= len;
-
   cur = head;
-  while (k > 0) {
+
+  if (k > len / 2)
+    return rotateLeft(head, len - k);
+
+  while (k-- > 0) {
     while (cur.next) {
       prev = cur;
       cur = cur.next;
@@ -25,7 +27,50 @@ const rotateRight = (head, k) => {
     cur.next = head; // cur is essentially the current tail
     prev.next = null;
     head = cur;
-    k--;
+  }
+  return head;
+};
+
+const rotateLeft = (head, k) => {
+  let cur = head;
+  while (cur.next) {
+    prev = cur;
+    cur = cur.next;
+  }
+  let tail = cur;
+
+  while (k-- > 0) {
+    tail.next = head;
+    tail = head;
+    head = head.next;
+    tail.next = null;
+  }
+  return head;
+};
+
+// O( (k % n) * n + n )? worst case is if k % n is n - 1, so time is (n - 1) * n
+const rotateRight1 = (head, k) => {
+  if (!head || !head.next) return head;
+
+  let prev;
+  let cur = head;
+  let len = 0;
+
+  while (cur) {
+    cur = cur.next;
+    len++;
+  }
+  k %= len;
+  cur = head;
+
+  while (k-- > 0) {
+    while (cur.next) {
+      prev = cur;
+      cur = cur.next;
+    }
+    cur.next = head; // cur is essentially the current tail
+    prev.next = null;
+    head = cur;
   }
   return head;
 };
