d2bf91cae684f5d65acaab43ab90cf9badb614e3
Michael Shi <michaelshi03@gmail.com>
2021-12-07 22:02:58 -0800

    Condense previous solution to 'Remove Covered Intervals'

--- leetcode/medium/removeCoveredIntervals.js
+++ leetcode/medium/removeCoveredIntervals.js
@@ -1,7 +1,7 @@
 // 1288. Remove Covered Intervals
 // https://leetcode.com/problems/remove-covered-intervals/
 
-// after sorting, the only situation in which an interval is uncovered is if both curInt[0] > left && curInt[1] > right
+// after sorting by the first el of intervals, the only situation in which an interval is uncovered is if both curInt[0] > left && curInt[1] > right
 // [ cur0, cur1 ] = curInt
 // 5 possible situations (would be 6 if intervals weren't all unique): cur0 can be = or > left, and cur1 can be >, <, or = right
   // = <, = >, > <. > =, > > (last one is only situation where it's uncovered)
@@ -10,8 +10,10 @@
     // we only update left if both cur0 > left & cur1 > right, since if cur1 <= right then the old left will still cover more space
 // since all intervals are unique, there will be at least 1 uncovered interval (so we can start left & right at -1)
 
+// O(n)
 const removeCoveredIntervals = intervals => {
   intervals.sort((a, b) => a[0] - b[0]);
+  // left & right represent the interval that currently covers the most space, where it is still possible that future intervals could fall inside
   let left = -1;
   let right = -1;
   let uncovered = 0;
@@ -24,6 +26,24 @@ const removeCoveredIntervals = intervals => {
     }
     if (cur1 > right)
       right = cur1;
+  }
+  return uncovered;
+};
+
+// similar to above, just condensed
+const removeCoveredIntervals = intervals => {
+  intervals.sort((a, b) => a[0] - b[0]);
+  let uncovered = 0;
+
+  for (let left = -1, right = -1, i = 0; i < intervals.length; i++) {
+    const [ cur0, cur1 ] = intervals[i];
+    if (cur1 > right) {
+      if (cur0 > left) {
+        uncovered++;
+        left = cur0;
+      }
+      right = cur1;
+    }
   }
   return uncovered;
 };
