2184da476033f86d97ae0473ba18739328edd84e
Michael Shi <michaelshi03@gmail.com>
2021-12-09 12:00:38 -0800

    Add alternate approach to 'Merge Intervals'

--- leetcode/medium/mergeIntervals.js
+++ leetcode/medium/mergeIntervals.js
@@ -9,9 +9,8 @@ const merge = intervals => {
   for (let i = 0; i < intervals.length; i++) {
     const cur = intervals[i];
 
-    while (i < intervals.length) {
+    while (i < intervals.length - 1) {
       const next = intervals[i + 1];
-      if (!next) break;
 
       if (next[0] <= cur[1]) {
         if (next[1] > cur[1])
@@ -24,5 +23,27 @@ const merge = intervals => {
   return res;
 };
 
-const arr = [[1,4],[1,4]];
+const arr = [ [ 1, 4 ],[ 1, 4 ] ];
 console.log(merge(arr));
+
+
+// old solution, similar to above approach
+const merge = intervals => {
+  intervals.sort((a, b) => a[0] - b[0]);
+  const res = [ intervals[0] ];
+
+  let ptr = 0;
+  for (let i = 1; i < intervals.length; i++) {
+    const curInter = intervals[i];
+    const resInter = res[ptr];
+
+    if (curInter[0] <= resInter[1]) {
+      if (curInter[1] > resInter[1])
+        resInter[1] = curInter[1];
+    } else {
+      res.push(curInter);
+      ptr++;
+    }
+  }
+  return res;
+};
