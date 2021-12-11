d8eee4f718259243fbeb35aa7b6031ce5dd3b699
Michael Shi <michaelshi03@gmail.com>
2021-12-10 20:11:14 -0800

    Review & implement 'Add Two Numbers'

--- leetcode/medium/addTwoNumbers.js
+++ leetcode/medium/addTwoNumbers.js
@@ -1,8 +1,11 @@
-var addTwoNumbers = function(l1, l2) {
+// 2. Add Two Numbers
+// https://leetcode.com/problems/add-two-numbers/
+
+const addTwoNumbers = (l1, l2) => {
   let p = l1;
   let q = l2;
-  let head = new ListNode();
-  let curr = head;
+  let prehead = new ListNode(null);
+  let curr = prehead;
   let carry = 0;
 
   while (p || q || carry) {
@@ -14,5 +17,34 @@ var addTwoNumbers = function(l1, l2) {
       q = q ? q.next : null;
   }
 
-  return head.next;
+  return prehead.next;
+};
+
+// a less succinct solution, but still O( Math.max(m, n) )
+const addTwoNumbers = (l1, l2) => {
+  let p = l1;
+  let q = l2;
+  let carry = 0;
+  let prehead = new ListNode(null);
+  let cur = prehead;
+
+  while (p || q) {
+    let sum = (p ? p.val : 0) + (q ? q.val : 0);
+    if (carry) {
+      sum += carry;
+      carry = 0;
+    }
+    if (sum >= 10) {
+      sum -= 10;
+      carry = 1;
+    }
+    cur.next = new ListNode(sum);
+    cur = cur.next;
+    if (p) p = p.next;
+    if (q) q = q.next;
+  }
+  if (carry)
+    cur.next = new ListNode(1);
+
+  return prehead.next;
 };
