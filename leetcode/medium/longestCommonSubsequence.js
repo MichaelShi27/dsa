da580d6b4bdc307e1093b5f399241fac272d498c
Michael Shi <michaelshi03@gmail.com>
2021-12-18 23:39:37 -0800

    Implement dynamic programming approach to 'Longest Common Subsequence'

--- leetcode/medium/longestCommonSubsequence.js
+++ leetcode/medium/longestCommonSubsequence.js
@@ -3,17 +3,33 @@
 
 
 const longestCommonSubsequence = (str1, str2) => {
-  const dp = new Array(str1.length + 1);
-  dp.fill( new Array(str2.length + 1).fill(0) );
-  console.log(dp);
+  const dp = []; // if I use .fill(), I get a bug that seems inherent to .fill()
+  for (let i = 0; i < str1.length + 1; i++) {
+    const row = [];
+    for (let j = 0; j < str2.length + 1; j++)
+      row.push(0);
+    dp.push(row);
+  }
+
+  for (let row = str1.length - 1; row >= 0; row--)
+    for (let col = str2.length - 1; col >= 0; col--)
+      if (str1[row] === str2[col])
+        dp[row][col] = 1 + dp[row + 1][col + 1];
+      else
+        dp[row][col] = Math.max( dp[row][col + 1], dp[row + 1][col] );
+
+  return dp[0][0];
 };
 
-// naive recursive solution w/o DP => exceeds time limit but passes 17/44 test cases
-// O(2^n)?
-const longestCommonSubsequence = (str1, str2) => {
-  if (!str1 || !str2) return 0;
+// console.log( longestCommonSubsequence("abcba", "abcbcba") );
+console.log( longestCommonSubsequence("abcde", "ace") );
 
-  return str1[0] === str2[0]
-    ? 1 + longestCommonSubsequence( str1.slice(1), str2.slice(1) );
-    : Math.max( longestCommonSubsequence(str1, str2.slice(1)), longestCommonSubsequence(str1.slice(1), str2) );
-};
+// // naive recursive solution w/o DP => exceeds time limit but passes 17/44 test cases
+// // O(2^n)?
+// const longestCommonSubsequence = (str1, str2) => {
+//   if (!str1 || !str2) return 0;
+
+//   return str1[0] === str2[0]
+//     ? 1 + longestCommonSubsequence( str1.slice(1), str2.slice(1) );
+//     : Math.max( longestCommonSubsequence(str1, str2.slice(1)), longestCommonSubsequence(str1.slice(1), str2) );
+// };
