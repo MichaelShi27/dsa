9eafa05116e4d264bbc77ce6b3c21f4abb39e5c3
Michael Shi <michaelshi03@gmail.com>
2021-12-19 18:08:38 -0800

    Implement alternate memoized approach to 'Longest Common Subsequence'

--- leetcode/medium/longestCommonSubsequence.js
+++ leetcode/medium/longestCommonSubsequence.js
@@ -3,7 +3,8 @@
 
 // O(2 * m * n) time, O(m * n) space
 // bottom-up approach (can also do top-down? i.e. upper left of chart to bottom right)
-const longestCommonSubsequence = (str1, str2) => {
+// based off NeetCode video
+const longestCommonSubsequence4 = (str1, str2) => {
   const dp = []; // if I use .fill(), I get a bug that seems inherent to .fill()
   for (let i = 0; i < str1.length + 1; i++) {
     const row = [];
@@ -23,7 +24,36 @@ const longestCommonSubsequence = (str1, str2) => {
 };
 
 // console.log( longestCommonSubsequence("abcba", "abcbcba") );
-console.log( longestCommonSubsequence("abcde", "ace") );
+
+
+// memoized approach based off https://www.youtube.com/watch?v=sSno9rV8Rhg
+// O(m * n)
+const longestCommonSubsequence3 = (str1, str2) => {
+  const dp = []; // if I use .fill(), I get a bug that seems inherent to .fill()
+  for (let i = 0; i < str1.length + 1; i++) {
+    const row = [];
+    for (let j = 0; j < str2.length + 1; j++)
+      row.push(null);
+    dp.push(row);
+  }
+
+  const recurse = (row, col) => {
+    if (row === str1.length || col === str2.length)
+      dp[row][col] = 0;
+    else if (str1[row] === str2[col]) {
+      const temp = dp[row + 1][col + 1] || recurse(row + 1, col + 1);
+      dp[row][col] = 1 + temp;
+    } else {
+      const temp1 = dp[row][col + 1] || recurse(row, col + 1);
+      const temp2 = dp[row + 1][col] || recurse(row + 1, col);
+      dp[row][col] = Math.max(temp1, temp2);
+    }
+    return dp[row][col];
+  };
+  return recurse(0, 0);
+};
+
+console.log( longestCommonSubsequence3("abcde", "ace") );
 
 /*
 - attempted memoized recursive solution => passes 42/44 test cases but gets this error msg:
@@ -31,7 +61,7 @@ console.log( longestCommonSubsequence("abcde", "ace") );
     what():  std::bad_alloc
 - the test case it fails has massive string inputs => perhaps the key eventually gets too large to store in the object?
 */
-const longestCommonSubsequence = (str1, str2, seen = {}) => {
+const longestCommonSubsequence2 = (str1, str2, seen = {}) => {
   if (!str1 || !str2) return 0;
 
   let res;
@@ -48,9 +78,9 @@ const longestCommonSubsequence = (str1, str2, seen = {}) => {
 };
 
 // // naive recursive solution w/o DP => exceeds time limit after 17/44 test cases
-// // O(2^n)?
+// // exponential time
 // // top-down approach
-// const longestCommonSubsequence = (str1, str2) => {
+// const longestCommonSubsequence1 = (str1, str2) => {
 //   if (!str1 || !str2) return 0;
 
 //   return str1[0] === str2[0]
