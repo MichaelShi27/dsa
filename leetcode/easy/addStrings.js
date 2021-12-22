9bf197254a44631fb2cb6f38fe0a35cea318a3f2
Michael Shi <michaelshi03@gmail.com>
2021-12-21 22:33:56 -0800

    Add alternate approach to 'Add Strings'

--- leetcode/easy/addStrings.js
+++ leetcode/easy/addStrings.js
@@ -1,6 +1,7 @@
 // 415. Add Strings
 // https://leetcode.com/problems/add-strings/
 
+// O( max(n, m) )
 const addStrings = (num1, num2) => {
   let carry = 0;
   let sumStr = '';
@@ -19,4 +20,23 @@ const addStrings = (num1, num2) => {
     sumStr = `${curSum}${sumStr}`;
   }
   return sumStr;
+};
+
+
+// 2-pointer approach
+// O( max(n, m) )
+const addStrings = (num1, num2) => {
+  let i = num1.length - 1;
+  let j = num2.length - 1;
+  let carry = 0;
+  let sum = '';
+
+  for (; i >= 0 || j >= 0 || carry; i--, j--) {
+    const digit1 = num1[i] - '0' || 0;
+    const digit2 = num2[j] - '0' || 0;
+    const tempSum = digit1 + digit2 + carry;
+    sum = `${tempSum % 10}${sum}`;
+    carry = Math.floor(tempSum / 10);
+  }
+  return sum;
 };
