fe83de57266664d1852dd997a331a25cbba82d7a
Michael Shi <michaelshi03@gmail.com>
2021-12-23 18:21:42 -0800

    Implement alternate solution to 'Plus One'

--- leetcode/easy/plusOne.js
+++ leetcode/easy/plusOne.js
@@ -20,6 +20,21 @@ const plusOne = digits => {
   return digits;
 };
 
+// similar to above, but w/ for-loop instead of do-while-loop
+const plusOne = digits => {
+  let carry = 1;
+  for (let i = digits.length - 1; i >= 0 && carry; i--) {
+    carry = 0;
+    digits[i]++;
+    if (digits[i] === 10) {
+      carry = 1;
+      digits[i] = 0;
+    }
+  }
+  if (carry) digits.unshift(1);
+  return digits;
+};
+
 // a more "naive" approach w/o a carry var, same space & time
 const plusOne = digits => {
   for (let i = digits.length - 1; i >= 0; i--)
