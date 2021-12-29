44a34b77ff071a612fd52ced4afb2b11e7c32dba
Michael Shi <michaelshi03@gmail.com>
2021-12-28 22:31:32 -0800

    Implement recursive divide-and-conquer approach to 'Majority Element'

--- leetcode/easy/majorityElement.js
+++ leetcode/easy/majorityElement.js
@@ -2,7 +2,7 @@
 // https://leetcode.com/problems/majority-element
 
 // hash table approach: n time, n space
-const majorityElement = nums => {
+const majorityElement1 = nums => {
   const obj = {};
   const half = nums.length / 2;
 
@@ -13,14 +13,14 @@ const majorityElement = nums => {
 };
 
 // .sort() approach: n log n time, const space
-const majorityElement = nums => {
+const majorityElement2 = nums => {
   nums.sort((a, b) => a - b);
   const mid = Math.floor(nums.length / 2);
   return nums[mid];
 };
 
 // random element approach: on average n time, worst case runs forever; const space
-const majorityElement = nums => {
+const majorityElement3 = nums => {
   while (true) {
     const randIdx = Math.floor( Math.random() * nums.length );
     if (isMajorityElement(nums[randIdx], nums))
@@ -38,3 +38,30 @@ const isMajorityElement = (num, nums) => {
   }
   return false;
 };
+
+// recursive divide & conquer approach:
+// faster when 'countInRange' functionality is kept inside 'getMajElOfSubarray'
+const majorityElement4 = nums => {
+  const getMajElOfSubarray = (lo, hi) => {
+    if (hi === lo)
+      return nums[lo]
+    const mid = Math.floor((lo + hi) / 2);
+    const leftRes = getMajElOfSubarray(lo, mid);
+    const rightRes = getMajElOfSubarray(mid + 1, hi);
+    if (leftRes === rightRes)
+      return leftRes;
+    return countInRange(leftRes, lo, mid) > countInRange(rightRes, mid + 1, hi) ? leftRes : rightRes;
+  };
+
+  const countInRange = (num, lo, hi) => {
+    let ct = 0;
+    for (let i = lo; i <= hi; i++)
+      if (nums[i] === num)
+        ct++;
+    return ct;
+  };
+  return getMajElOfSubarray(0, nums.length - 1);
+};
+
+const arr = [ 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2 ];
+console.log(majorityElement4(arr));
