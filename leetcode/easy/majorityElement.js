88ee3f6626cc244260866d658f85882857a14d34
Michael Shi <michaelshi03@gmail.com>
2021-12-28 21:38:04 -0800

    Implement random-element approach to 'Majority Element'

--- leetcode/easy/majorityElement.js
+++ leetcode/easy/majorityElement.js
@@ -1,23 +1,7 @@
-/**
- *
-Given an array nums of size n, return the majority element.
+// 169. Majority Element
+// https://leetcode.com/problems/majority-element
 
-The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
-
-Example 1:
-
-Input: nums = [3,2,3]
-Output: 3
-Example 2:
-
-Input: nums = [2,2,1,1,1,2,2]
-Output: 2
-
- * @param {number[]} nums
- * @return {number}
- */
-
-// O(n)
+// hash table approach: n time, n space
 const majorityElement = nums => {
   const obj = {};
   const half = nums.length / 2;
@@ -28,9 +12,29 @@ const majorityElement = nums => {
   }
 };
 
-// O(n log n)
+// .sort() approach: n log n time, const space
 const majorityElement = nums => {
   nums.sort((a, b) => a - b);
   const mid = Math.floor(nums.length / 2);
   return nums[mid];
+};
+
+// random element approach: on average n time, worst case runs forever; const space
+const majorityElement = nums => {
+  while (true) {
+    const randIdx = Math.floor( Math.random() * nums.length );
+    if (isMajorityElement(nums[randIdx], nums))
+      return nums[randIdx];
+  }
+};
+
+const isMajorityElement = (num, nums) => {
+  let ct = 0;
+  for (const el of nums) {
+    if (el === num)
+      ct++;
+    if (ct > nums.length / 2)
+      return true;
+  }
+  return false;
 };
