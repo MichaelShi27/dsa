27dfc7f41ae9f3e474542016c3807169951fda72
Michael Shi <michaelshi03@gmail.com>
2022-01-21 14:13:23 -0800

    Implement alternate divide & conquer approach to 'Majority Element'

--- leetcode/easy/majorityElement.js
+++ leetcode/easy/majorityElement.js
@@ -51,6 +51,9 @@ const majorityElement4 = nums => {
     if (leftRes === rightRes)
       return leftRes;
     return countInRange(leftRes, lo, mid) > countInRange(rightRes, mid + 1, hi) ? leftRes : rightRes;
+    // the above return statement passes the tests, but I feel it should check whole subarray (i.e. lo to hi for both left & right) instead of just its own half.
+    // indeed, the official LC solution uses the whole subarray - maybe the LC tests just aren't extensive enough? I feel there are edge cases where the above
+    // line won't work, but I couldn't come up with any
   };
 
   const countInRange = (num, lo, hi) => {
@@ -65,3 +68,38 @@ const majorityElement4 = nums => {
 
 const arr = [ 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2 ];
 console.log(majorityElement4(arr));
+
+/*
+recursive divide & conquer approach where helper func returns 2 values instead of 1:
+- I thought this would be better than the above solution bc it wouldn't have to "recount" if the left & right returned elements weren't equal,
+since the left & right halves would also theoretically return the # of occurrences of their respective elements and I could just compare them directly.
+However, I ended up still having to count them (i.e. newLeftCt & newRightCt) since otherwise incorrect counts are passed up the recursive stack, leading
+to incorrect results
+*/
+const majorityElement5 = nums => {
+  const countInRange = (num, lo, hi) => {
+    let ct = 0;
+    for (let i = lo; i <= hi; i++)
+      if (nums[i] === num)
+        ct++;
+    return ct;
+  };
+
+  const getMajElOfSubarray = (lo, hi) => {
+    if (hi === lo)
+      return [ nums[lo], 1 ];
+    const mid = Math.floor((lo + hi) / 2);
+    const [ leftMajEl, leftCt ] = getMajElOfSubarray(lo, mid);
+    const [ rightMajEl, rightCt ] = getMajElOfSubarray(mid + 1, hi);
+    if (leftMajEl === rightMajEl)
+      return [ leftMajEl, leftCt + rightCt ];
+    const newLeftCt = countInRange(leftMajEl, lo, mid);
+    const newRightCt = countInRange(rightMajEl, mid + 1, hi);
+    return newLeftCt > newRightCt ? [ leftMajEl, newLeftCt ] : [ rightMajEl, newRightCt ];
+  };
+  return getMajElOfSubarray(0, nums.length - 1)[0];
+};
+
+const arr = [ 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2 ];
+console.log(majorityElement6(arr));
+
