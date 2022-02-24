da5a6a2efc4af5e76f6b326822e0084652826c48
Michael Shi <michaelshi03@gmail.com>
2022-02-24 14:48:51 -0800

    Implement alternate solutions for 'Linked List Cycle'

--- leetcode/easy/linkedListCycle.js
+++ leetcode/easy/linkedListCycle.js
@@ -1,6 +1,8 @@
 // 141. Linked List Cycle
 // https://leetcode.com/problems/linked-list-cycle/
 
+// floyd's algo (2 pointers/fast & slow/tortois & hare)
+// n time, const space
 const hasCycle = head => {
   if (!head) return false;
 
@@ -11,6 +13,30 @@ const hasCycle = head => {
     slow = slow.next;
     fast = fast.next.next;
     if (fast === slow) return true;
+  }
+  return false;
+};
+
+// set => n time & space
+const hasCycle = head => {
+  const set = new Set();
+  let cur = head;
+
+  while (cur) {
+    if (set.has(cur)) return true;
+    set.add(cur);
+    cur = cur.next;
+  }
+  return false;
+};
+
+// n time, n space?
+const hasCycle = head => {
+  let cur = head;
+  while (cur) {
+    if (cur.seen) return true;
+    cur.seen = true;
+    cur = cur.next;
   }
   return false;
 };
