2e6337659db2e7a72ddba07d17ed478ae537b474
Michael Shi <michaelshi03@gmail.com>
2022-03-16 12:52:21 -0700

    Review & implement 'Roman to Integer'

--- leetcode/easy/romanToInteger.js
+++ leetcode/easy/romanToInteger.js
@@ -1,64 +1,44 @@
-// Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+// 13. Roman to Integer
+// https://leetcode.com/problems/roman-to-integer/
 
-// Symbol       Value
-// I             1
-// V             5
-// X             10
-// L             50
-// C             100
-// D             500
-// M             1000
-// For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+const map = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
 
-// Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+// // n time, const space
+// const romanToInt = s => {
+//   let sum = 0;
 
-// I can be placed before V (5) and X (10) to make 4 and 9.
-// X can be placed before L (50) and C (100) to make 40 and 90.
-// C can be placed before D (500) and M (1000) to make 400 and 900.
-// Given a roman numeral, convert it to an integer.
+//   for (let i = 0; i < str.length; i++)
+//     if ( map[str[i]] < map[str[i + 1]] )
+//       sum -= map[str[i]];
+//     else
+//       sum += map[str[i]];
 
+//   return sum;
+// };
 
+// pure recursive approach
+const romanToInt = str => (
+  str.length === 1
+    ? map[str[0]]
+    : romanToInt(str.slice(1)) + map[str[0]] * ( (map[str[0]] < map[str[1]]) ? -1 : 1 )
+);
 
-// Example 1:
+// // recursive w helper
+// const romanToInt = s => {
+//   let sum = 0;
 
-// Input: s = "III"
-// Output: 3
-// Example 2:
+//   const helper = str => {
+//     if (!str.length) return;
+//     if ( map[str[0]] < map[str[1]] )
+//       sum -= map[str[0]];
+//     else
+//       sum += map[str[0]];
+//     helper(str.slice(1));
+//   };
+//   helper(s);
+//   return sum;
+// };
 
-// Input: s = "IV"
-// Output: 4
-// Example 3:
-
-// Input: s = "IX"
-// Output: 9
-// Example 4:
-
-// Input: s = "LVIII"
-// Output: 58
-// Explanation: L = 50, V= 5, III = 3.
-// Example 5:
-
-// Input: s = "MCMXCIV"
-// Output: 1994
-// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
-
-
-// Constraints:
-
-// 1 <= s.length <= 15
-// s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
-// It is guaranteed that s is a valid roman numeral in the range [1, 3999].
-
-var romanToInt = function(s) {
-  const obj = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
-  let sum = 0;
-
-  for (let i = 0; i < str.length; i++) {
-    if ( obj[str[i]] < obj[str[i + 1]] )
-      sum -= obj[str[i]];
-    else
-      sum += obj[str[i]];
-  }
-
-  return sum;
-};
+console.log( romanToInt('LVIII') === 58 );
+console.log( romanToInt('MCMXCIV') === 1994 );
+// console.log(3 < undefined);
