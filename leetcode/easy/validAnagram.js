6e48e5c0f93599d1003fa5bdcfc8911e57e88327
Michael Shi <michaelshi03@gmail.com>
2022-03-16 13:25:35 -0700

    Review & implement 'Valid Anagram'

--- leetcode/easy/validAnagram.js
+++ leetcode/easy/validAnagram.js
@@ -1,27 +1,8 @@
-/*
+// 242. Valid Anagram
+// https://leetcode.com/problems/valid-anagram/
 
-Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-
-
-
-Example 1:
-
-Input: s = "anagram", t = "nagaram"
-Output: true
-Example 2:
-
-Input: s = "rat", t = "car"
-Output: false
-
-
-Constraints:
-
-1 <= s.length, t.length <= 5 * 104
-s and t consist of lowercase English letters.
-
-*/
-
-var isAnagram = function(s, t) {
+// n time, n space
+const isAnagram = (s, t) => {
   if (s.length !== t.length)
     return false;
 
@@ -32,8 +13,47 @@ var isAnagram = function(s, t) {
     obj[t[i]] = (obj[t[i]] || 0) - 1;
   }
 
-  for (let key in obj)
-    if (obj[key] !== 0)
+  for (const key in obj)
+    if (obj[key])
+      return false;
+
+  return true;
+};
+
+// sort approach
+// n log n time (bc sort), n space (bc split)?
+const isAnagram = (s, t) => {
+  if (s.length !== t.length)
+    return false;
+
+  s = s.split('');
+  t = t.split('');
+  s.sort();
+  t.sort();
+
+  for (let i = 0; i < s.length; i++)
+    if (s[i] !== t[i])
+      return false;
+
+  return true;
+};
+
+// double obj approach
+// n time, n space (or const space since only set # of possible chars?)
+const isAnagram = (s, t) => {
+  if (s.length !== t.length)
+    return false;
+
+  const sObj = {};
+  const tObj = {};
+
+  for (const char of s)
+    sObj[char] = ++sObj[char] || 1;
+  for (const char of t)
+    tObj[char] = ++tObj[char] || 1;
+
+  for (const key in sObj)
+    if (tObj[key] !== sO[key])
       return false;
 
   return true;
