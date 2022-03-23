b7356f20a52f08ba54fdc50ba9444c6df9b08af4
Michael Shi <michaelshi03@gmail.com>
2022-03-23 01:41:00 -0700

    Review & implement 'Two Sum'

--- leetcode/easy/twoSum.js
+++ leetcode/easy/twoSum.js
@@ -1,28 +1,6 @@
 // 1. Two Sum
 // https://leetcode.com/problems/two-sum/
 
-// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-
-// You may assume that each input would have exactly one solution, and you may not use the same element twice.
-
-// You can return the answer in any order.
-
-
-
-// Example 1:
-
-// Input: nums = [2,7,11,15], target = 9
-// Output: [0,1]
-// Output: Because nums[0] + nums[1] == 9, we return [0, 1].
-// Example 2:
-
-// Input: nums = [3,2,4], target = 6
-// Output: [1,2]
-// Example 3:
-
-// Input: nums = [3,3], target = 6
-// Output: [0,1]
-
 // // O(n) hash table solution
 // const twoSum = (nums, target) => {
 //   const obj = {};
@@ -38,7 +16,7 @@
 const twoSum = (nums, target) => {
   const obj = {};
   const found = [];
-  const numsCopy = nums.slice();
+  const numsCopy = nums.slice(); // need this var & 2nd for-loop to remember & return original indices
   const sortedNums = nums.sort((a, b) => a - b);
 
   for (let i = 0, j = sortedNums.length - 1; true; i++, j--) {
@@ -59,15 +37,15 @@ const twoSum = (nums, target) => {
   }
 
   let firstIdx, secondIdx;
-  for (let i = 0; firstIdx == undefined || secondIdx === undefined; i++) {
+  for (let i = 0; firstIdx == undefined || secondIdx === undefined; i++)
     if (firstIdx === undefined && numsCopy[i] === found[0])
       firstIdx = i;
     else if (numsCopy[i] === found[1])
       secondIdx = i;
-  }
-  return [ firstIdx, secondIdx ];
+
+      return [ firstIdx, secondIdx ];
 };
 
-// console.log( twoSum([ -18, 12, 3, 0 ], -6) ); // [ 0, 1 ]
+console.log( twoSum([ -18, 12, 3, 0 ], -6) ); // [ 0, 1 ]
 console.log( twoSum([ 3, 3 ], 6) ); // [ 0, 1 ]
-// console.log( twoSum([ 3, 2, 4 ], 6) ); // [ 1, 2 ]
+console.log( twoSum([ 3, 2, 4 ], 6) ); // [ 1, 2 ]
