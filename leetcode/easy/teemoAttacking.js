fdbf487c12db78d472d8e2d928909a3eb5fb0941
Michael Shi <michaelshi03@gmail.com>
2022-04-03 01:17:02 -0700

    Review & implement 'Teemo Attacking'

--- leetcode/easy/teemoAttacking.js
+++ leetcode/easy/teemoAttacking.js
@@ -1,54 +1,33 @@
-/*
-Our hero Teemo is attacking an enemy Ashe with poison attacks! When Teemo attacks Ashe, Ashe gets poisoned for a exactly duration seconds. More formally, an attack at second t will mean Ashe is poisoned during the inclusive time interval [t, t + duration - 1]. If Teemo attacks again before the poison effect ends, the timer for it is reset, and the poison effect will end duration seconds after the new attack.
+// 495. Teemo Attacking
+// https://leetcode.com/problems/teemo-attacking/submissions/
 
-You are given a non-decreasing integer array timeSeries, where timeSeries[i] denotes that Teemo attacks Ashe at second timeSeries[i], and an integer duration.
+const findPoisonedDuration = (arr, duration) => {
+  let sum = 0;
 
-Return the total number of seconds that Ashe is poisoned.
-
-
-
-Example 1:
-
-Input: timeSeries = [1,4], duration = 2
-Output: 4
-Explanation: Teemo's attacks on Ashe go as follows:
-- At second 1, Teemo attacks, and Ashe is poisoned for seconds 1 and 2.
-- At second 4, Teemo attacks, and Ashe is poisoned for seconds 4 and 5.
-Ashe is poisoned for seconds 1, 2, 4, and 5, which is 4 seconds in total.
-Example 2:
-
-Input: timeSeries = [1,2], duration = 2
-Output: 3
-Explanation: Teemo's attacks on Ashe go as follows:
-- At second 1, Teemo attacks, and Ashe is poisoned for seconds 1 and 2.
-- At second 2 however, Teemo attacks again and resets the poison timer. Ashe is poisoned for seconds 2 and 3.
-Ashe is poisoned for seconds 1, 2, and 3, which is 3 seconds in total.
-
-
-Constraints:
-
-1 <= timeSeries.length <= 104
-0 <= timeSeries[i], duration <= 107
-timeSeries is sorted in non-decreasing order.
-*/
-
-var findPoisonedDuration = function(arr, dur) {
-  let ct = 0;
-
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] + dur - 1 >= arr[i + 1])
-      ct += arr[i + 1] - arr[i];
+  for (let i = 0; i < arr.length; i++)
+    if (arr[i] + duration - 1 >= arr[i + 1])
+      sum += arr[i + 1] - arr[i];
     else
-      ct += dur;
-  }
-  return ct;
+      sum += duration;
+
+  return sum;
 };
 
+const findPoisonedDuration = (arr, duration) => {
+  let sum = 0;
 
-// alt
-var findPoisonedDuration = function(arr, dur) {
-  let ct = 0;
+  for (let i = 0; i < arr.length; i++) {
+    const cur = arr[i];
+    const next = arr[i + 1];
+    sum += (cur + duration > next) ? (next - cur) : duration;
+  }
+
+  return sum;
+};
+
+const findPoisonedDuration = (arr, duration) => {
+  let sum = 0;
   for (let i = 0; i < arr.length - 1; i++)
-    ct += Math.min( dur, arr[i + 1] - arr[i] );
-  return ct + dur;
+    sum += Math.min( duration, arr[i + 1] - arr[i] );
+  return sum + duration;
 };
