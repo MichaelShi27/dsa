92cb146cdf57815960e99d8e4621e650f0e7d80a
Michael Shi <michaelshi03@gmail.com>
2022-04-27 00:28:32 -0700

    Review & implement 'Lowest Common Ancestor of a Binary Search Tree'

--- leetcode/easy/lowestCommonAncestor.js
+++ leetcode/easy/lowestCommonAncestor.js
@@ -1,43 +1,39 @@
-/*
-235. Lowest Common Ancestor of a Binary Search Tree
-https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+// 235. Lowest Common Ancestor of a Binary Search Tree
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
 
-Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
-
-According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
-*/
-
-
-// dfs
-var lowestCommonAncestor = function(root, p, q) {
-  const higherVal = Math.max(p.val, q.val);
-  const lowerVal = higherVal === p.val ? q.val : p.val;
-
-  if (
-    root.val === higherVal || root.val === lowerVal ||
-    (root.val < higherVal && root.val > lowerVal)
-  ) return root;
-  else if (root.val > higherVal)
+// recursive, n time & space?
+const lowestCommonAncestor = (root, p, q) => {
+  if (root.val > p.val && root.val > q.val)
     return lowestCommonAncestor(root.left, p, q);
-  else // if (root.val < lowerVal)
+  if (root.val < p.val && root.val < q.val)
     return lowestCommonAncestor(root.right, p, q);
+  return root; // if root.val = either, or is between them
 };
 
-// bfs
-var lowestCommonAncestor = function(root, p, q) {
-  const queue = [ root ];
-  const higherVal = Math.max(p.val, q.val);
-  const lowerVal = higherVal === p.val ? q.val : p.val;
-
-  while (queue.length) {
-    const curr = queue.shift();
-    if (
-      curr.val === higherVal || curr.val === lowerVal ||
-      (curr.val < higherVal && curr.val > lowerVal)
-    ) return curr;
-    else if (curr.val > higherVal)
-      curr.left && queue.push(curr.left);
-    else // if (curr.val < lowerVal)
-      curr.right && queue.push(curr.right);
-  }
+// iterative, n time const space?
+const lowestCommonAncestor = (root, p, q) => {
+  while (true)
+    if (root.val < p.val && root.val < q.val)
+      root = root.right;
+    else if (root.val > p.val && root.val > q.val)
+      root = root.left;
+    else
+      return root;
 };
+
+// not a good solution, bc we have to check isDescendant (n time) n times
+const lowestCommonAncestor = (root, p, q) => {
+  let curLowest = null;
+
+  if (isDescendant(root, p) && isDescendant(root, q))
+    curLowest = root;
+
+  if (root.val < p.val && root.val < q.val) // only need to check this "half" of tree if both nodes are on that side
+    curLowest = lowestCommonAncestor(root.right, p, q) || curLowest;
+  else if (root.val > p.val && root.val > q.val)
+    curLowest = lowestCommonAncestor(root.left, p, q) || curLowest;
+
+  return curLowest;
+};
+
+const isDescendant = (root, node) => root && ( root === node || isDescendant(root.left, node) || isDescendant(root.right, node) );
