248fa343adf81352a4a3b19e915d1a8e2c88c2ba
Michael Shi <michaelshi03@gmail.com>
2022-05-11 01:02:04 -0700

    Review & implement 'Range Sum of BST'

--- leetcode/easy/rangeSumOfBST.js
+++ leetcode/easy/rangeSumOfBST.js
@@ -1,32 +1,43 @@
-/*
-https://leetcode.com/problems/range-sum-of-bst/
+// 938. Range Sum of BST
+// https://leetcode.com/problems/range-sum-of-bst
 
-Given the root node of a binary search tree and two integers low and high, return the sum of values of all nodes with a value in the inclusive range [low, high].
-*/
-
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @param {number} low
- * @param {number} high
- * @return {number}
- */
- var rangeSumBST = function(root, low, high) {
+// dfs pure
+const rangeSumBST = ({ val, left, right }, low, high) => {
   let sum = 0;
-  const recurse = node => {
-    if (!node) return;
-    if (node.val <= high && node.val >= low)
-      sum += node.val;
-    recurse(node.left);
-    recurse(node.right);
+  if (val <= high && val >= low)
+    sum += val;
+  if (left && (val > low))
+    sum += rangeSumBST(left, low, high);
+  if (right && (val < high))
+    sum += rangeSumBST(right, low, high);
+  return sum;
+};
+
+// dfs w/ recursive helper
+const rangeSumBST = (root, low, high) => {
+  let sum = 0;
+
+  const recurse = ({ val, left, right }) => {
+    (val <= high) && (val >= low) && (sum += val);
+    left && (val > low) && recurse(left);
+    right && (val < high) && recurse(right);
   };
+
   recurse(root);
+  return sum;
+};
+
+// bfs iter
+const rangeSumBST = (root, low, high) => {
+  let sum = 0;
+  const q = [ root ];
+
+  while (q.length) {
+    const { val, left, right } = q.shift();
+    if (val <= high && val >= low)
+      sum += val;
+    left && (val > low) && q.push(left); // better to check here than to just push in null & check at the top of loop, bc then we can avoid constantly .shift-ing
+    right && (val < right) && q.push(right);
+  }
   return sum;
 };
