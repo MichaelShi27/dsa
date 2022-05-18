b740a43d3dbe3c48e050fe1bbbdce88511f0c24c
Michael Shi <michaelshi03@gmail.com>
2022-05-17 20:03:50 -0700

    Review & implement 'Happy Number'

--- leetcode/easy/happyNumber.js
+++ leetcode/easy/happyNumber.js
@@ -1,34 +1,7 @@
-// Write an algorithm to determine if a number n is happy.
+// 202. Happy Number
+// https://leetcode.com/problems/happy-number/
 
-// A happy number is a number defined by the following process:
-
-// Starting with any positive integer, replace the number by the sum of the squares of its digits.
-// Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
-// Those numbers for which this process ends in 1 are happy.
-// Return true if n is a happy number, and false if not.
-
-
-
-// Example 1:
-
-// Input: n = 19
-// Output: true
-// Explanation:
-// 12 + 92 = 82
-// 82 + 22 = 68
-// 62 + 82 = 100
-// 12 + 02 + 02 = 1
-// Example 2:
-
-// Input: n = 2
-// Output: false
-
-
-// Constraints:
-
-// 1 <= n <= 231 - 1
-
-// iterative
+// iterative - obj store method
 const isHappy = n => {
   let sum = 0;
   const store = {};
@@ -36,14 +9,14 @@ const isHappy = n => {
   while (sum !== 1 && !store[sum]) {
     store[sum] = true;
     sum = 0;
-    for (let num of n.toString())
+    for (const num of n.toString())
       sum += num ** 2;
     n = sum;
   }
   return sum === 1;
 };
 
-// iter
+// iterative, similar to above
 const isHappy = n => {
   let sum = 0;
   const store = {};
@@ -60,15 +33,7 @@ const isHappy = n => {
   return sum === 1;
 };
 
-const getNext = n => {
-  let sum = 0;
-  while (n) {
-    sum += (n % 10) ** 2;
-    n = Math.floor(n / 10);
-  }
-  return sum;
-};
-
+// iterative, floyd's algo
 const isHappy = n => {
   let fast = getNext(n);
   let slow = n;
@@ -79,12 +44,20 @@ const isHappy = n => {
   return fast === 1;
 };
 
+const getNext = n => {
+  let sum = 0;
+  while (n) {
+    sum += (n % 10) ** 2;
+    n = Math.floor(n / 10);
+  }
+  return sum;
+};
+
 // recursive
 const isHappy = (n, store = {}) => {
   let sum = 0;
-  n = n.toString();
 
-  for (let num of n)
+  for (const num of n.toString())
     sum += num ** 2;
 
   if (store[sum])
@@ -93,5 +66,5 @@ const isHappy = (n, store = {}) => {
     store[sum] = true;
 
   if (sum === 1) return true;
-  return isHappy( sum.toString(), store );
+  return isHappy(sum, store);
 };
