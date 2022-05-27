d08ffd15fae09e03c67a5d72b0d81eb2e944c89b
Michael Shi <michaelshi03@gmail.com>
2022-05-26 21:50:24 -0700

    Review & implement 'Climbing Stairs'

--- leetcode/easy/climbStairs.js
+++ leetcode/easy/climbStairs.js
@@ -1,32 +1,6 @@
 // 70. Climbing Stairs
 // https://leetcode.com/problems/climbing-stairs/
 
-// You are climbing a staircase. It takes n steps to reach the top.
-
-// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
-
-// Example 1:
-
-// Input: n = 2
-// Output: 2
-// Explanation: There are two ways to climb to the top.
-// 1. 1 step + 1 step
-// 2. 2 steps
-// Example 2:
-
-// Input: n = 3
-// Output: 3
-// Explanation: There are three ways to climb to the top.
-// 1. 1 step + 1 step + 1 step
-// 2. 1 step + 2 steps
-// 3. 2 steps + 1 step
-
-
-// Constraints:
-
-// 1 <= n <= 45
-
-
 /*
 APPROACH:
 - solve for first few cases (e.g. n = 1-4) and look for pattern
@@ -56,22 +30,30 @@ const climbStairs2 = n => {
   return arr[n];
 };
 
+// iterative - similar to tabulation method, but uses fibonacci sequence (the # of steps actually follows fibonacci)
+const climbStairs4 = n => {
+  let prev = 0, cur = 1;
+  for (let i = 2; i <= n; i++)
+    [ prev, cur ] = [ cur, prev + cur ];
+  return cur;
+};
+
 // memoization => this approach is like approach 1, just going 0 to n instead of n to 0. this approach sort of simulates "real life" -
 // at each step, I can take 1 or 2 steps, and if I go past the end (whether using 1 or 2 steps) it doesn't count, while if I hit the end
 // exactly, it counts
 const climbStairs3 = n => {
   const seen = {};
 
-  const recurse = (curStep, goal) => {
-    if (curStep > goal) return 0;
-    if (curStep === goal) return 1;
+  const recurse = (curStep) => {
+    if (curStep > n) return 0;
+    if (curStep === n) return 1;
     if (seen[curStep]) return seen[curStep];
 
-    const res = recurse(curStep + 1, goal) + recurse(curStep + 2, goal);
+    const res = recurse(curStep + 1) + recurse(curStep + 2);
     seen[curStep] = res;
     return res;
   };
-  return recurse(0, n);
+  return recurse(0);
 };
 
 const func = climbStairs1;
