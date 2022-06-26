c780fec8a6ed17d82bba31351fa904cccd51ca14
Michael Shi <michaelshi03@gmail.com>
2022-06-25 22:01:28 -0700

    Review & implement 'Container With Most Water'

--- leetcode/medium/containerWithMostWater.js
+++ leetcode/medium/containerWithMostWater.js
@@ -1,5 +1,5 @@
 // 11. Container With Most Water
-https://leetcode.com/problems/container-with-most-water/
+// https://leetcode.com/problems/container-with-most-water/
 
 const maxArea = heights => {
   let maxArea = 0;
@@ -7,8 +7,10 @@ const maxArea = heights => {
   for (let i = 0, j = heights.length - 1; i < j;) {
     curArea = (j - i) * Math.min(heights[i], heights[j]);
     maxArea = Math.max(maxArea, curArea);
-    if (heights[i] < heights[j]) i++;
-    else j--;
+    if (heights[i] < heights[j])
+      i++;
+    else
+      j--;
   }
   return maxArea;
 };
