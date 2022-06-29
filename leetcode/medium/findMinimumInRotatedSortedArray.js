9218d5af212f78fbec7d7c18520db035711badf2
Michael Shi <michaelshi03@gmail.com>
2022-06-28 18:20:31 -0700

    Review & implement 'Find Minimum in Rotated Sorted Array'

--- leetcode/medium/findMinimumInRotatedSortedArray.js
+++ leetcode/medium/findMinimumInRotatedSortedArray.js
@@ -3,17 +3,7 @@
 // 153. Find Minimum in Rotated Sorted Array
 // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 
-// Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
-
-// [4,5,6,7,0,1,2] if it was rotated 4 times.
-// [0,1,2,4,5,6,7] if it was rotated 7 times.
-// Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
-
-// Given the sorted rotated array nums of unique elements, return the minimum element of this array.
-
-// You must write an algorithm that runs in O(log n) time.
-
-
+// LC solution - similar to mine
 const findMin = nums => {
   const len = nums.length;
   if (len === 1 || nums[len - 1] > nums[0]) return nums[0];
@@ -34,4 +24,52 @@ const findMin = nums => {
     else
       start = mid + 1;
   }
+};
+
+// pure recursive - can also just pass the pointers instead of slicing array each time
+const findMin = nums => {
+  const end = nums.length - 1,
+    mid = Math.floor(end / 2);
+
+  if (nums[0] <= nums[end])
+    return nums[0];
+  if (nums[mid] > nums[mid + 1])
+    return nums[mid + 1];
+  return nums[0] > nums[mid]
+    ? findMin( nums.slice(0, mid + 1) )
+    : findMin( nums.slice(mid) );
+};
+
+// iterative => log n time (binary search)
+const findMin = nums => {
+  let left = 0,
+    right = nums.length - 1,
+    mid;
+
+  while (true) {
+    mid = Math.floor( (left + right) / 2 );
+
+    if (nums[left] <= nums[right])
+      return nums[left];
+    if (nums[mid] > nums[mid + 1])
+      return nums[mid + 1];
+    if (nums[left] > nums[mid])
+      right = mid;
+    else
+      left = mid;
+  }
+};
+
+// from LC Discussion tab
+const findMin = (nums) => {
+  let l = 0;
+  let r = nums.length - 1;
+  while (l < r) {
+    const m = Math.floor( (l + r) / 2 );
+    if (nums[m] > nums[r])
+      l = m + 1;
+    else
+      r = m;
+  }
+  return nums[l];
 };
