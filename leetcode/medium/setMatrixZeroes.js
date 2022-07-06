e26f08664b83d8077921c147b83a536f76999007
Michael Shi <michaelshi03@gmail.com>
2022-07-05 20:27:06 -0700

    Review & implement 'Set Matrix Zeroes'

--- leetcode/medium/setMatrixZeroes.js
+++ leetcode/medium/setMatrixZeroes.js
@@ -16,8 +16,8 @@ const setZeroes = matrix => {
   /*
   The loops below can also be 1 nested loop going through the entire matrix, checking every spot w/r/t the 2 objects.
   I think 2 loops are more efficient than nested loops when there aren't a lot of 0s, but not as efficient when there are a lot
-    - e.g. just 3 0s => time is 3n + 3m rather than n * m
-    - e.g. all 0s => time is 2(n * m) rather than n * m
+    - e.g. just 3 0s => 3n + 3m operations rather than n * m
+    - e.g. all 0s => 2(n * m) operations rather than n * m
   */
   for (const row in rows)
     for (let c = 0; c < matrix[0].length; c++)
@@ -31,7 +31,10 @@ const setZeroes = matrix => {
 // n * m time, const space
 // use first item of each row/col as flag instead of using 2 objects
 const setZeroes = matrix => {
-  let firstColHasZero, firstRowHasZero; // needed bc first row & col share a first item, i.e. matrix[0][0], so what if one has 0 but other doesn't?
+  // below vars are needed bc first row & col share a first item, i.e. matrix[0][0], so if one has 0 but other doesn't,
+  // the one that doesn't will still get overwritten w/ 0s if we don't account for it. we can also have a single flag var
+  // to track if either first row or first col should be 0s, & use the actual matrix[0][0] for the other one
+  let firstColHasZero, firstRowHasZero;
 
   for (let c = 0; c < matrix[0].length; c++)
     if (matrix[0][c] === 0)
@@ -50,7 +53,7 @@ const setZeroes = matrix => {
 
   // fill rows & cols
   for (let c = 1; c < matrix[0].length; c++)
-    for (let r = 1; r < matrix.length; r++) {
+    for (let r = 1; r < matrix.length; r++)
       if (matrix[r][0] === 0 || matrix[0][c] === 0)
         matrix[r][c] = 0;
 
