d2968dc31961b11fd292176cb07ee3e860834a26
Michael Shi <michaelshi03@gmail.com>
2022-07-09 19:06:16 -0700

    Review & implement 'Group Anagrams'

--- leetcode/medium/groupAnagrams.js
+++ leetcode/medium/groupAnagrams.js
@@ -2,33 +2,38 @@
 // https://leetcode.com/problems/group-anagrams/
 
 // uses array of letter counts as key in obj
+// n * m (m being avg length of str in strs))
 const groupAnagrams = strings => {
   const obj = {};
+
   for (const str of strings) {
     const letterCounts = new Array(26).fill(0);
+
     for (const char of str) {
       const idx = char.charCodeAt() - 97;
       letterCounts[idx]++;
     }
-    if (obj[letterCounts])
-      obj[letterCounts].push(str);
-    else
-      obj[letterCounts] = [ str ];
+
+    const key = JSON.stringify(letterCounts); // can also use .join w/ a separator (otherwise [ 0, 1, 0, 10 ] and [ 0, 10, 1, 0 ] will get mixed up), or just letterCounts on its own
+    if (!obj[key])
+      obj[key] = [];
+    obj[key].push(str);
   }
   return Object.values(obj);
 };
 
 // // uses sorted string as key in obj
-// const groupAnagrams = strings => {
-//   const obj = {};
-//   for (const str of strings) {
-//     const sortedStr = str.split('').sort();
-//     if (obj[sortedStr])
-//       obj[sortedStr].push(str);
-//     else
-//       obj[sortedStr] = [ str ];
+// // n * m log m (m being avg length of str in strs)
+// const groupAnagrams = strs => {
+//   const seen = {};
+
+//   for (const str of strs) {
+//     const sorted = str.split('').sort().join('');
+//     if (!seen[sorted])
+//       seen[sorted] = [];
+//     seen[sorted].push(str);
 //   }
-//   return Object.values(obj);
+//   return Object.values(seen);
 // };
 
 console.log( groupAnagrams(["eat","tea","tan","ate","nat","bat"]) );
