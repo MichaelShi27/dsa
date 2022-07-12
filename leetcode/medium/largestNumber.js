3a165cafea21fcf292c1c3b95d3e2f5151e54426
Michael Shi <michaelshi03@gmail.com>
2022-07-11 19:52:54 -0700

    Review & implement 'Largest Number'

--- leetcode/medium/largestNumber.js
+++ leetcode/medium/largestNumber.js
@@ -1,16 +1,9 @@
 // 179. Largest Number
 // https://leetcode.com/problems/largest-number/
 
-// from Discussion page
 const largestNumber = nums => {
-  if (!nums || nums.length === 0)
-    return '0';
-
   nums.sort((a, b) => `${b}${a}` - `${a}${b}`);
-
-  if (nums[0] === 0)
-    return '0';
-  return nums.join('');
+  return (nums[0] === 0) ? '0' : nums.join('');
 };
 
 
