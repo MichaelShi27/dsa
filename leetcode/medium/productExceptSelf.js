7c9304b292284cadfe57c0014abc314b5d0e4ed1
Michael Shi <michaelshi03@gmail.com>
2022-07-12 12:28:31 -0700

    Review & implement 'Product of Array Except Self'

--- leetcode/medium/productExceptSelf.js
+++ leetcode/medium/productExceptSelf.js
@@ -1,54 +1,55 @@
-// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+// 238. Product of Array Except Self
+// https://leetcode.com/problems/product-of-array-except-self/
 
-// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+// 2nd time - n space
+const productExceptSelf = nums => {
+  const arr1 = [];
+  const arr2 = [];
 
-// You must write an algorithm that runs in O(n) time and without using the division operation.
+  let prod = 1;
+  for (const num of nums) {
+    prod *= num;
+    arr1.push(prod);
+  }
+  prod = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    prod *= nums[i];
+    arr2[i] = prod;
+  }
 
+  const res = [];
+  for (let i = 0; i < nums.length; i++)
+    res[i] = (arr1[i - 1] ?? 1) * (arr2[i + 1] ?? 1);
 
-
-// Example 1:
-
-// Input: nums = [1,2,3,4]
-// Output: [24,12,8,6]
-// Example 2:
-
-// Input: nums = [-1,1,0,-3,3]
-// Output: [0,0,9,0,0]
-
-
-// Constraints:
-
-// 2 <= nums.length <= 105
-// -30 <= nums[i] <= 30
-// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
-
-// linear space
-var productExceptSelf = function(nums) {
-    const left = [];
-    const right = new Array(nums.length);
-
-    for (let i = 0; i < nums.length; i++) {
-        if (i === 0)
-            left.push(1);
-        else
-            left.push( left[i - 1] * nums[i - 1] );
-    }
-    for (let i = nums.length - 1; i >= 0; i--) {
-        if (i === nums.length - 1)
-            right[i] = 1;
-        else
-            right[i] = right[i + 1] * nums[i + 1];
-    }
-
-    const res = [];
-    for (let i = 0; i < nums.length; i++)
-        res.push( left[i] * right[i] );
-
-    return res;
+  return res;
 };
 
-// 2
-var productExceptSelf = function(nums) {
+// linear space
+const productExceptSelf = nums => {
+  const left = [];
+  const right = new Array(nums.length);
+
+  for (let i = 0; i < nums.length; i++)
+    if (i === 0)
+      left.push(1);
+    else
+      left.push( left[i - 1] * nums[i - 1] );
+
+  for (let i = nums.length - 1; i >= 0; i--)
+    if (i === nums.length - 1)
+      right[i] = 1;
+    else
+      right[i] = right[i + 1] * nums[i + 1];
+
+  const res = [];
+  for (let i = 0; i < nums.length; i++)
+    res.push( left[i] * right[i] );
+
+  return res;
+};
+
+// similar to above, but uses 1 arr
+const productExceptSelf = nums => {
   const left = [];
 
   for (let i = 0; i < nums.length; i++)
@@ -72,7 +73,7 @@ var productExceptSelf = function(nums) {
 
 
 // constant space, similar to above
-var productExceptSelf = function(nums) {
+const productExceptSelf = nums => {
   const res = [ 1 ];
 
   for (let i = 1; i < nums.length; i++)
@@ -90,20 +91,19 @@ var productExceptSelf = function(nums) {
 
 // can also do O(1) space and a single loop, fill in right & left at same time
 
-
 // someone else's solution:
-const productExceptSelf = function(nums) {
-    const output = [];
-    let leftMult = 1;
-    let rightMult = 1;
+const productExceptSelf = nums => {
+  const output = [];
+  let leftMult = 1;
+  let rightMult = 1;
 
-    for (let i = nums.length - 1, j = 0; i >= 0; i--, j++) {
-        output[i] = (output[i] ?? 1) * rightMult;
-        output[j] = (output[j] ?? 1) * leftMult;
+  for (let i = nums.length - 1, j = 0; i >= 0; i--, j++) {
+    output[i] = (output[i] ?? 1) * rightMult;
+    output[j] = (output[j] ?? 1) * leftMult;
 
-        rightMult *= nums[i];
-        leftMult *= nums[j];
-    }
+    rightMult *= nums[i];
+    leftMult *= nums[j];
+  }
 
-    return output;
+  return output;
 };
