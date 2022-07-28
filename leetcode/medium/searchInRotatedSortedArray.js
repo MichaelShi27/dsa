c7993dc368c51384b090b59def6ffe097f287873
Michael Shi <michaelshi03@gmail.com>
2022-07-28 01:08:40 -0700

    Review & implement 'Search in Rotated Sorted Array'

--- leetcode/medium/searchInRotatedSortedArray.js
+++ leetcode/medium/searchInRotatedSortedArray.js
@@ -3,29 +3,6 @@
 // 33. Search in Rotated Sorted Array
 // https://leetcode.com/problems/search-in-rotated-sorted-array/
 
-
-// There is an integer array nums sorted in ascending order (with distinct values).
-
-// Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
-
-// Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
-
-// You must write an algorithm with O(log n) runtime complexity.
-
-
-// Example 1:
-
-// Input: nums = [4,5,6,7,0,1,2], target = 0
-// Output: 4
-// Example 2:
-
-// Input: nums = [4,5,6,7,0,1,2], target = 3
-// Output: -1
-// Example 3:
-
-// Input: nums = [1], target = 0
-// Output: -1
-
 const search = (arr, num) => {
   let low = 0;
   let high = arr.length - 1;
@@ -89,3 +66,47 @@ const findPivotIdx = arr => { // essentially the same as ./findMinimumInRotatedS
 // console.log( search([ 1, 3 ], 3) === 1 );
 // console.log( search([ 1, 3, 5 ], 3) === 1 );
 // console.log( search([ 6 ], 1) === -1 );
+
+// 2nd time
+const search = (nums, target) => {
+  if (nums.length === 1)
+    return nums[0] === target ? 0 : -1;
+
+  let pivot = (nums[0] < nums[nums.length - 1]) ? 0 : findPivot(nums);
+  let lo = 0, hi = nums.length - 1;
+
+  if (pivot !== 0) {
+    if (target >= nums[0] && target <= nums[pivot - 1])
+      hi = pivot;
+    else
+      lo = pivot;
+  }
+
+
+  while (lo <= hi) {
+    const mid = Math.floor( (lo + hi) / 2 );
+    if (nums[mid] === target)
+      return mid;
+    else if (nums[mid] > target)
+      hi = mid - 1;
+    else
+      lo = mid + 1;
+  };
+  return -1;
+};
+
+const findPivot = nums => {
+  let lo = 0, hi = nums.length - 1;
+
+  while (true) {
+    const mid = Math.floor( (lo + hi) / 2 );
+    if (nums[lo] < nums[hi])
+      return lo;
+    else if (nums[mid] > nums[mid + 1])
+      return mid + 1;
+    else if (nums[lo] > nums[mid])
+      hi = mid;
+    else // nums[lo] < nums[mid]
+      lo = mid;
+  }
+};
