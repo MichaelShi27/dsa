533ec67f65caf632885611f89b33051b35925112
Michael Shi <michaelshi03@gmail.com>
2022-11-17 23:22:28 -0800

    Review & implement 'Largest Number'

--- leetcode/medium/largestNumber.js
+++ leetcode/medium/largestNumber.js
@@ -6,6 +6,12 @@ const largestNumber = nums => {
   return (nums[0] === 0) ? '0' : nums.join('');
 };
 
+const largestNumber = nums => {
+  const sortFunc = (a, b) => (`${b}${a}` > `${a}${b}`) ? 1 : -1;
+  nums.sort(sortFunc);
+  return nums[0] === 0 ? '0' : nums.join('');
+};
+
 
 let arr = [ 1, 1, 2, 3, 9, 1, 22, 8 ];
 console.log( largestNumber(arr) );
