3e01a8da7a62d0ea3337d1a4ba34c6b91a527221
Michael Shi <michaelshi03@gmail.com>
2022-11-20 18:19:04 -0800

    Review & implement 'Find the Index of the First Occurrence in a String'

--- leetcode/medium/findTheIndexOfTheFirstOccurrenceInAString.js.js
+++ leetcode/medium/findTheIndexOfTheFirstOccurrenceInAString.js.js
@@ -1,14 +1,15 @@
-// 28. Implement strStr()
-// https://leetcode.com/problems/implement-strstr/
+// 28. Find the Index of the First Occurrence in a String
+// https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
 
 // naive - n * m time
 const strStr = (haystack, needle) => {
   if (needle === '') return 0;
   if (haystack.length < needle.length) return -1;
 
-  for (let i = 0; i < haystack.length; i++)
+  for (let i = 0; i <= haystack.length - needle.length; i++)
     for (let j = 0; j < needle.length; j++)
-      if (haystack[i + j] !== needle[j]) break;
+      if (haystack[i + j] !== needle[j]) 
+        break;
       else if (j === needle.length - 1)
         return i;
 
